e6d1166ed9cde022c58c78b049268b41fa7ef9e8
sunpeijia <1311107143@qq.com>
2016-10-29 20:57:02 +0800

    no message

--- minigame/galdemo/resource.js
+++ minigame/galdemo/resource.js
@@ -1,196 +1,71 @@
 var ScriptList = {
-    0:{
-    "character": [],
-    "background": "image/bk_library",
-    "name": "",
-    "dialogue": "this is ... 上便是一种reactive能力。而类似这种能自动对外部环境的变化作出响应的系统我们称之为反应型系统",
-    "toindex":1
-},
-    1:{
-    "character": [{
-        "body": "image/mikaru_front_1",
-        "face": "image/mikaru_front_exp_1"
-    }],
-    "background": "image/bk_library",
-    "name": "udspj1",
-    "dialogue": "t化作出响应的系统我们称之为反应型系统",
-    "toindex":2
-},
-    2:{
-    "character": [{
-        "body": "image/mikaru_front_1",
-        "face": "image/mikaru_front_exp_1"
-    }],
-    "background": "image/bk_library",
-    "name": "udspj1",
-    "dialogue": "",
-    "toindex":999,
+    0: {
+        "character": [],
+        "background": "image/bk_library",
+        "name": "",
+        "dialogue": "this is ... 上便是一种reactive能力。而类似这种能自动对外部环境的变化作出响应的系统我们称之为反应型系统",
+        "toindex": 1
+    },
+    1: {
+        "character": [{
+            "body": "image/mikaru_front_1",
+            "face": "image/mikaru_front_exp_1"
+        }],
+        "background": "image/bk_library",
+        "name": "udspj1",
+        "dialogue": "t化作出响应的系统我们称之为反应型系统",
+        "toindex": 2
+    },
+    2: {
+        "character": [{
+            "body": "image/mikaru_front_1",
+            "face": "image/mikaru_front_exp_1"
+        }],
+        "background": "image/bk_library",
+        "name": "udspj1",
+        "dialogue": "",
+        "toindex": 999,
         "selections": [{ "choice": "选项 xiang 1", "toindex": "3" },
-        { "choice": "动对外部环", "toindex": "4" },
-        { "choice": "xuan xiang 2", "toindex": "5" }
-    ]
-},
-    3:{
-    "character": [{
-        "body": "image/mikaru_front_1",
-        "face": "image/mikaru_front_exp_1"
-    }],
-    "background": "image/bk_library",
-    "name": "udspj1",
-    "dialogue": "t化作出响444444444应的系统我们称之为反应型系统",
-    "toindex":0
-},
-    4:{
-    "character": [{
-        "body": "image/mikaru_front_1",
-        "face": "image/mikaru_front_exp_1"
-    }],
-    "background": "image/bk_library",
-    "name": "udspj1",
-    "dialogue": "t化作出响应的5555555555系统我们称之为反应型系统",
-    "toindex":0
-},
-    5:{
-    "character": [{
-        "body": "image/mikaru_front_1",
-        "face": "image/mikaru_front_exp_1"
-    }],
-    "background": "image/bk_library",
-    "name": "udspj1",
-    "dialogue": "t化作出响应的系统我666666666666们称之为反应型系统",
-    "toindex":110
+            { "choice": "动对外部环", "toindex": "4" },
+            { "choice": "xuan xiang 2", "toindex": "5" }
+        ]
+    },
+    3: {
+        "character": [{
+            "body": "image/mikaru_front_1",
+            "face": "image/mikaru_front_exp_1"
+        }],
+        "background": "image/bk_library",
+        "name": "udspj1",
+        "dialogue": "t化作出响444444444应的系统我们称之为反应型系统",
+        "toindex": 0
+    },
+    4: {
+        "character": [{
+            "body": "image/mikaru_front_1",
+            "face": "image/mikaru_front_exp_1"
+        }],
+        "background": "image/bk_library",
+        "name": "udspj1",
+        "dialogue": "t化作出响应的5555555555系统我们称之为反应型系统",
+        "toindex": 0
+    },
+    5: {
+        "character": [{
+            "body": "image/mikaru_front_1",
+            "face": "image/mikaru_front_exp_1"
+        }],
+        "background": "image/bk_library",
+        "name": "udspj1",
+        "dialogue": "t化作出响应的系统我666666666666们称之为反应型系统",
+        "toindex": 110
+    }
 }
-}
 
 
 
-// [{
-    // "index": 0,
-    // "character": [{
-    //     "body": "image/mikaru_front_1",
-    //     "face": "image/mikaru_front_exp_1"
-    // }],
-    // "background": "image/bk_library",
-    // "name": "udspj1",
-    // "dialogue": "this is ... 上便是一种reactive能力。而类似这种能自动对外部环境的变化作出响应的系统我们称之为反应型系统"
-// }
-// , {
-//     "index": 1,
-//     "character": [{
-//         "body": "chara1",
-//         "face": "chara1exp1"
-//     }],
-//     "background": "bk1",
-//     "name": "udspj1",
-//     "dialogue": "实际上便是一种reactive能力。而类似这种能自动对外部环境的变化作出响应的系统我们称之为反应型系统(Reactive System)。典型的外部环境变化包括外部输入信号的变化、事件的发生，而且系统的响应通常是实时的。互动系统是最重要的一种反应型系统，它不但能响应外部环境的变化，还会将自己的内部状态通过某种方式反馈给外部观察者。"
-// }
-// , {
-//     "index": 2,
-//     "character": [{
-//         "body": "chara1",
-//         "face": "chara1exp1"
-//     }, {
-//         "body": "chara2",
-//         "face": "chara1exp1"
-//     }],
-//     "background": "bk2",
-//     "name": "udspj2",
-//     "dialogue": "aaaaaaaaaaaa?"
-// }, {
-//     "index": 3,
-//     "character": [{
-//         "body": "chara1",
-//         "face": "chara1exp1"
-//     }, {
-//         "body": "chara2",
-//         "face": "chara1exp1"
-//     }],
-//     "background": "bk2",
-//     "name": "udspj2",
-//     "dialogue": "3333333333?"
-//     ,
-//     "selections": [{ "choice": "选项 xiang 1", "toindex": "4" },
-//         { "choice": "动对外部环", "toindex": "5" },
-//         { "choice": "xuan xiang 2", "toindex": "6" }
-//     ]
-// }, {
-//     "index": 4,
-//     "character": [{
-//         "body": "chara2",
-//         "face": "chara1exp1"
-//     }],
-//     "background": "bk1",
-//     "name": "udspj2",
-//     "dialogue": "3333333333?"
-// }, {
-//     "index": 5,
-//     "character": [{
-//         "body": "chara1",
-//         "face": "chara1exp1"
-//     }, {
-//         "body": "chara2",
-//         "face": "chara1exp1"
-//     }],
-//     "background": "bk2",
-//     "name": "udspj2",
-//     "dialogue": "而且系统的响应通常是实时的。互动系统是最重要的一种反应型系统，它不但能响应外部环境的变化，还会将自己的内部状态通过某种方式反馈给外部观察者。?"
-// }, {
-//     "index": 6,
-//     "character": [{
-//         "body": "chara1",
-//         "face": "chara1exp1"
-//     }, {
-//         "body": "chara2",
-//         "face": "chara1exp1"
-//     }],
-//     "background": "bk2",
-//     "name": "udspj2",
-//     "dialogue": "3333333333?"
-// }, {
-//     "index": 7,
-//     "character": [{
-//         "body": "chara1",
-//         "face": "chara1exp1"
-//     }],
-//     "background": "bk1",
-//     "name": "udspj2",
-//     "dialogue": "便是一种reactive能力。而类似这种能自动对外部环境的变化作出响应的系统我们称之为反应型系统(Rea?"
-// }, {
-//     "index": 8,
-//     "character": [{
-//         "body": "chara1",
-//         "face": "chara1exp1"
-//     }, {
-//         "body": "chara2",
-//         "face": "chara1exp1"
-//     }],
-//     "background": "bk2",
-//     "name": "udspj2",
-//     "dialogue": "实际上便是一种reactive能力。而类似这种能自动对外部环境的变化作出响应的系统我们称之为反应型系统(Reactive System)。典型的外部环境变化包括外部输入信号的变化、事件的发生，而且系统的响应通常是实时的。互动系统是最重要的一种反应型系统，它不但能响应外部环境的变化，还会将自己的内部状态通过某种方式反馈给外部观察者。"
-// }, {
-//     "index": 9,
-//     "character": [{
-//         "body": "chara1",
-//         "face": "chara1exp1"
-//     }, {
-//         "body": "chara2",
-//         "face": "chara1exp1"
-//     }],
-//     "background": "bk2",
-//     "name": "udspj2",
-//     "dialogue": "what?"
-// }, {
-//     "index": 10,
-//     "character": [{
-//         "body": "chara1",
-//         "face": "chara1exp1"
-//     }, {
-//         "body": "chara2",
-//         "face": "chara1exp1"
-//     }],
-//     "background": "bk2",
-//     "name": "udspj2",
-//     "dialogue": "3333333333?"
-// }
 
-// ];
+
+
+
+
